48beb72e23bfbc40cecc7f97eb576e903264d2e9
Douglas Obara <dobara@kabarak.ac.ke>
2021-06-05 17:11:55 +0300

    Handling Events, Binding Event and Updating the state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,33 +3,40 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3", "tag4"],
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0)
-      return (
-        <p>
-          <h1>There are no tags</h1>
-        </p>
-      );
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
 
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
   render() {
     return (
       <React.Fragment>
-        {this.state.tags.length === 0 && "please create a new tag"}
-        {this.renderTags()} // conditional rendering
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "btn btn-sm m-2 btn-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state; // object destructuring
+    return count === 0 ? "Zero" : count; // ternary operator
   }
 }
 
